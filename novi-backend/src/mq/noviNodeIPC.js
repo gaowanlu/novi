1fcd36c9147e3cbabfcb6034861a2e0b5e9bf6fd
gaowanlu <2209120827@qq.com>
2025-11-08 09:18:21 +0000

    update: backend noviNodeIPC

--- novi-backend/src/mq/noviNodeIPC.js
+++ novi-backend/src/mq/noviNodeIPC.js
@@ -6,12 +6,39 @@ let noviNodeIPC = {
     init() {
         mqKafkaInit();
         mqRabbitMQInit((msgFromNoviNode) => {
-            logger.error(`mqRabbitMQ msgFromNoviNode ${msgFromNoviNode}`);
-            rabbitMQNoviNodeChannel.sendToNoviNode(1, "hello world");
+            try {
+                const json = JSON.parse(msgFromNoviNode);
+                if (json) {
+                    let message = JSON.parse(json.message);
+                    this.msgFromNoviNode(json.fromNode, json.event, message);
+                }
+            } catch (err) {
+                logger.error(`mqRabbitMQInit msgFromNoviNode ${err.message}`);
+            }
         });
+        // setInterval(() => {
+        //     this.sendToNode(1, this.createNewMessage("test", {
+        //         data: 'test data'
+        //     }));
+        // }, 1000);
     },
+
     sendToNode(noviNode, msg) {
-        rabbitMQNoviNodeChannel.sendToNoviNode(noviNode, "hello world");
+        let jsonStr = JSON.stringify(msg);
+        rabbitMQNoviNodeChannel.sendToNoviNode(noviNode, jsonStr);
+    },
+
+    createNewMessage(event, message) {
+        let JsonStr = JSON.stringify(message);
+        return {
+            fromNode: process.env.NOVI_NODE,
+            event,
+            message: JsonStr
+        };
+    },
+
+    msgFromNoviNode(fromNode, event, message) {
+        logger.error(`fromNode ${fromNode} event ${event} message ${JSON.stringify(message)}`);
     }
 };
 
